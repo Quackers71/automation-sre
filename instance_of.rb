1754b357743fd825eb44e2a56994ad7dcd526ca3
Q <robq71@gmail.com>
2020-08-11 14:52:59 +0100

    using the dot class method

---
+++ instance_of.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# Any method in Ruby that returns a boolean value after it is called has a name that ends in a question mark.
+10.instance_of?(Integer)
+
+# Introspection
+10.class
+"Hi".class
---
+++ objects.rb
@@ -0,0 +1,10 @@
+class Apple
+    attr_accessor :color, :flavor
+end
+
+gala = Apple.new
+gala.color = "red"
+gala.flavor = "sweet"
+
+puts gala.color
+puts gala.flavor
