9e2a343048bfa11d71faeb38af1e329c690fccbe
Q <robq71@gmail.com>
2020-08-11 21:52:41 +0100

    working with Methods

---
+++ groupedMethods.rb
@@ -0,0 +1,8 @@
+# calculate_intercept = [[2] * [0] + [1]]
+# to work on!
+
+y_intercept_1 = calculate_intercept(1, 2, 10)
+puts "The y intercept of the line is: #{y_intercept_1}"
+
+y_intercept_2 = calculate_intercept(35, 3, 4)
+puts "The y intercept of the line is: #{y_intercept_2}"
---
+++ independantMethods.rb
@@ -0,0 +1,13 @@
+x = 1
+b = 2
+m = 10
+y = m * x + b
+
+puts "The y intercept of the line is: "
+puts y
+
+m = 35
+y = m * x + b
+
+puts "The y intercept of the line is: "
+puts y
