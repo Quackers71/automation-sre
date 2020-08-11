e4bc74d53cf6a4697cf8f3dda7e85b6349758848
Q <robq71@gmail.com>
2020-08-11 17:24:23 +0100

    using arrays or hashes

---
+++ arrays_or_hashes.rb
@@ -0,0 +1,9 @@
+# using an arrary
+ip_addresses = ["192.168.1.1", "127.0.0.1", "8.8.8.8"]
+
+# using hashes - key value pairs
+host_addresses = {"router" => "192.168.1.1", "localhost" => "127.0.0.1", "google" => "8.8.8.8"}
+
+puts ip_addresses
+
+puts host_addresses
