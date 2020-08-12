f146a82ddc811246b820f79a4d0ae5c438dccbef
Q <robq71@gmail.com>
2020-08-12 11:29:03 +0100

    added comment for req_rel method

--- use_comma_joiner.rb
+++ use_comma_joiner.rb
@@ -1,4 +1,5 @@
 require "./super_useful_methods"
+# You could use this method - require_relative "super_useful_methods"
 
 a = ["this", "sure", "is", "a", "lot", "of", "commas"]
 puts comma_joiner(a)
